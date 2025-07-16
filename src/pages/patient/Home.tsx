5ffe5942f990def5c00f32ed1f991773b87fe866
spikree <avimahalingpure10@gmail.com>
2025-07-16 22:15:47 +0530

    implement search functionality for patients in home component

--- src/pages/patient/Home.tsx
+++ src/pages/patient/Home.tsx
@@ -1,9 +1,12 @@
 import { PatientStore } from "@/store/PatientStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Card } from "@/components/ui/card";
 import { TabsList } from "@radix-ui/react-tabs";
 import { Tabs, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import RenderDoctorAccordion from "@/components/renderDoctorAccordion";
+import { Input } from "@/components/ui/input";
+import { Search, X } from "lucide-react";
+import type { DoctorInterface } from "@/store/PatientStore";
+import RenderDoctorAccordion from "@/components/RenderDoctorAccordion";
 
 const Home = () => {
   const { getDoctorList, getLabResults, getPatientDetails, doctorList } =
@@ -14,6 +17,21 @@ const Home = () => {
     getLabResults();
     getPatientDetails();
   }, [getDoctorList, getLabResults, getPatientDetails]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [searchDoctorList, setSearchDoctorList] = useState<DoctorInterface[]>(
+    []
+  );
+
+  const clearSearch = () => {
+    setSearchQuery("");
+  };
+
+  useEffect(() => {
+    const filteredList = doctorList.filter((doctor) =>
+      doctor.doctor.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setSearchDoctorList(filteredList);
+  }, [searchQuery, doctorList]);
 
   const currentDoctorList = doctorList.filter(
     (doctor) => doctor.patientStatus === "current"
@@ -25,6 +43,32 @@ const Home = () => {
 
   return (
     <div>
+      <Card className=" max-w-auto max-h-full p-10 m-4 flex flex-wrap gap-2 justify-between">
+        <div className="flex gap-2 max-w-96">
+          <div className="relative w-full">
+            {searchQuery.length !== 0 ? (
+              <X
+                className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground w-4 h-4"
+                onClick={() => clearSearch()}
+              />
+            ) : (
+              <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground w-4 h-4" />
+            )}
+            <Input
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search Doctors by name"
+              className="pl-10"
+            />
+          </div>
+        </div>
+
+        {/* <div>
+            <DialogTrigger asChild>
+              <Button variant={"green"}>Add Patient</Button>
+            </DialogTrigger>
+          </div> */}
+      </Card>
       <Card className="max-w-auto max-h-full p-10 m-4">
         <Tabs defaultValue="current">
           <TabsList>
@@ -38,7 +82,13 @@ const Home = () => {
 
           <Card className="py-2 mt-4 border-0 shadow-none">
             <TabsContent value="current">
-              <RenderDoctorAccordion doctors={currentDoctorList} />
+              <RenderDoctorAccordion
+                doctors={
+                  searchDoctorList.length === 0
+                    ? currentDoctorList
+                    : searchDoctorList
+                }
+              />
             </TabsContent>
 
             <TabsContent value="old">
