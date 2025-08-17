9fcad2aec156f116315603880320d585d0563409
spikree <avimahalingpure10@gmail.com>
2025-08-17 21:43:26 +0530

    replaced auth user with isCheckingAuth

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,13 @@ import PatientAiSummary from "./pages/doctor/PatientAiSummary";
 import { Loader } from "lucide-react";
 
 const App = () => {
-  const { checkAuth, authUser } = useAuthStore();
+  const { checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  if (!authUser) {
+  if (isCheckingAuth) {
     return (
       <div className="flex items-center justify-center h-screen">
         <div className="flex flex-col items-center justify-center space-y-2">
