ada5d108a1e6c291c9540ab3c893cae63b2256c1
spikree <avimahalingpure10@gmail.com>
2025-08-26 17:30:57 +0530

    Added chat functionality to CommonStore: getMessages, sendMessage, and updated messages interface

--- src/store/CommonStore.ts
+++ src/store/CommonStore.ts
@@ -4,11 +4,27 @@ import { AxiosError } from "axios";
 // import type { AxiosResponse } from "axios";
 import { toast } from "sonner";
 
-interface CommonStore {
-  updateProfile: (bio: string, profilePicture: File) => Promise<void>;
+interface chatInterface {
+  _id: string;
+  senderId: string;
+  reveiverId: string;
+  text: string;
+  chatId: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export const CommonStore = create<CommonStore>(() => ({
+interface CommonStore {
+  updateProfile: (bio: string, profilePicture: File) => Promise<void>;
+  getMessages: (receiverId: string) => Promise<void>;
+  sendMessage: (receiverId: string, text: string) => Promise<void>;
+
+  messages: chatInterface[];
+}
+
+export const CommonStore = create<CommonStore>((set) => ({
+  messages: [],
+
   updateProfile: async (bio: string, profilePicture: File) => {
     const formData = new FormData();
 
@@ -30,6 +46,33 @@ export const CommonStore = create<CommonStore>(() => ({
       const axiosError = error as AxiosError<{ message: string }>;
       const errorMessage =
         axiosError.response?.data?.message || "Error updating profile";
+      toast.error(errorMessage);
+    }
+  },
+
+  getMessages: async (receiverId: string) => {
+    try {
+      const response = await axiosInstance.get(
+        `/chatRoute/getMessages/${receiverId}`
+      );
+      set({ messages: response.data.messages });
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      const errorMessage =
+        axiosError.response?.data?.message || "Error fetching messages";
+      toast.error(errorMessage);
+    }
+  },
+
+  sendMessage: async (receiverId: string, text: string) => {
+    try {
+      await axiosInstance.post(`/chatRoute/sendMessage/${receiverId}`, {
+        text,
+      });
+    } catch (error) {
+      const axiosError = error as AxiosError<{ message: string }>;
+      const errorMessage =
+        axiosError.response?.data?.message || "Error sending messages";
       toast.error(errorMessage);
     }
   },
