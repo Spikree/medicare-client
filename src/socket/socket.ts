a289b7420a0b840f58f7fb03ad07e2e80dee9ef3
Avi <avimahalingpure10@gmail.com>
2025-09-10 21:19:55 +0530

    testing if the new mac has git configured properly

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,3 +3,5 @@ import { io } from "socket.io-client";
 const URL = import.meta.env.VITE_BASE_URL;
 
 export const socket = io(URL,{autoConnect: false});
+
+// testing if the new mac has git configured properly
