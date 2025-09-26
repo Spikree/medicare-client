a1fa33eae568cada1f6776c31eae58d56fc34fec
Avi <avimahalingpure10@gmail.com>
2025-09-26 13:19:43 +0100

    removed file system build error

--- src/pages/doctor/Dashboard.tsx
+++ src/pages/doctor/Dashboard.tsx
@@ -1,4 +1,4 @@
-import AddNewPatientDialog from "@/components/addNewPatientDialog";
+import AddNewPatientDialog from "@/components/AddNewPatientDialog";
 import PatientAccordion from "@/components/PatientAccordian";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
